cd11750576015b5cc01e5fd89e84d88fdba65b93
riouxg <gabriel.rioux@hotmail.com>
2021-04-20 14:49:28 -0400

    4th commit

--- lib/azurespeech.rb
+++ lib/azurespeech.rb
@@ -1,25 +1,27 @@
 
   ###IDENTIFICATION
     def identifySpeaker
-        audiofile = File.open("app/assets/audio/test.wav") ##File.open("app/assets/audio/#{audioFileLoadedInView}")
-        connection = Excon.post('https://eastus.api.cognitive.microsoft.com/speaker/identification/v2.0/text-independent/profiles/identifySingleSpeaker?profileIds={profileIds}',
+        audiofile = File.open("app/assets/audio/test.wav") ##File.open("app/assets/audio/#{audioFileLoaded}")
+        speaker = Excon.post('https://eastus.api.cognitive.microsoft.com/speaker/identification/v2.0/text-independent/profiles/identifySingleSpeaker?profileIds={profileIds}',
         headers:{
             'Content-Type' => 'audio/wave',
             'Ocp-Apim-Subscription-Key' => "#{ENV['AZURE_KEY']}"
         },
         body: audiofile,
       )
+      return speaker.body
     end
 
     def createEnroll
-        connection = Excon.post('https://eastus.api.cognitive.microsoft.com/speaker/identification/v2.0/text-independent/profiles/{profileId}/enrollments',
+        audiofile = File.open("app/assets/audio/test.wav") ##File.open("app/assets/audio/#{audioFileLoaded}")
+        enroll = Excon.post('https://eastus.api.cognitive.microsoft.com/speaker/identification/v2.0/text-independent/profiles/{profileId}/enrollments',
         headers: {
-            'Content-Type' => 'application/json',
+            'Content-Type' => 'audio/wave',
             'Ocp-Apim-Subscription-Key' => "#{ENV['AZURE_KEY']}"
         },
-        body: JSON.generate("locale": 'en-us')
+        body: audiofile
      )
-      return connection.body
+      return enroll.body
     end
 
     def createProfile
@@ -32,12 +34,11 @@
           },
           body: JSON.generate("locale": 'en-us')
         )
-        response = connection['profileId']##??????????
       return connection.body
     end
 
     def getProfile
-      connection =
+      profil =
         Excon.get(
           'https://eastus.api.cognitive.microsoft.com/speaker/identification/v2.0/text-independent/profiles/"dc53bdab-feef-4310-aefd-b5f8581ddd6e",',
           headers: {
@@ -46,59 +47,58 @@
           },
           body: JSON.generate("locale": 'en-us')
         )
-        return connection.body['profile']####?????
+        return profil.body
     end
 
 
     ### VERIFICATION ##########################
 
-    def createEnroll(file_name, profile_id)
-        file = File.open(file path)
-        endpoint =
-          "https://eastus.api.cognitive.microsoft.com/speaker/verification/v2.0/text-independent/profiles/#{
-            profile_id
-          }/enrollments"
-    
-        begin
-          connection =
-            Excon.post(
-              endpoint,
-              debug_request: true,
-              debug_connection: true,
-              headers: {
-                'Content-Type' => 'audio/wave',
-                'Ocp-Apim-Subscription-Key' => "#{ENV['AZURE_KEY']}"
-              },
-              body: file,
-              instrumentor: ActiveSupport::Notifications
-            )
-          return connection.body
-        end
-    end
-  
-      def createProfile
-        connection =
-          Excon.post(
-            'https://eastus.api.cognitive.microsoft.com/speaker/verification/v2.0/text-independent/profiles',
-            headers: {
-              'Content-Type' => 'application/json',
-              'Ocp-Apim-Subscription-Key' => "#{ENV['AZURE_KEY']}"
-            },
-            body: JSON.generate("locale": 'en-us')
-          )
-        return connection.body
-      end
-  
-    def getProfile
-      connection =
-        Excon.get(
-          'https://eastus.api.cognitive.microsoft.com/speaker/verification/v2.0/text-independent/profiles',
-          headers: {
-            'Content-Type' => 'application/json',
-            'Ocp-Apim-Subscription-Key' => "#{ENV['AZURE_KEY']}"
-          },
-          body: JSON.generate("locale": 'en-us')
-        )
-      response = JSON.parse(connection.body)
-      return response[0]
-    end
+     def identifySpeaker
+      audiofile = File.open("app/assets/audio/test.wav") ##File.open("app/assets/audio/#{audioFileLoaded}")
+      speaker = Excon.post('https://eastus.api.cognitive.microsoft.com/speaker/verification/v2.0/text-independent/profiles/identifySingleSpeaker?profileIds={profileIds}',
+      headers:{
+          'Content-Type' => 'audio/wave',
+          'Ocp-Apim-Subscription-Key' => "#{ENV['AZURE_KEY']}"
+      },
+      body: audiofile,
+    )
+    return speaker.body
+  end
+
+  def createEnroll
+      audiofile = File.open("app/assets/audio/test.wav") ##File.open("app/assets/audio/#{audioFileLoaded}")
+      enroll = Excon.post('https://eastus.api.cognitive.microsoft.com/speaker/verification/v2.0/text-independent/profiles/{profileId}/enrollments',
+      headers: {
+          'Content-Type' => 'audio/wave',
+          'Ocp-Apim-Subscription-Key' => "#{ENV['AZURE_KEY']}"
+      },
+      body: audiofile
+   )
+    return enroll.body
+  end
+
+  def createProfile
+    connection =
+      Excon.post(
+        'https://eastus.api.cognitive.microsoft.com/speaker/verification/v2.0/text-independent/profiles',
+        headers: {
+          'Content-Type' => 'application/json',
+          'Ocp-Apim-Subscription-Key' => "#{ENV['AZURE_KEY']}"
+        },
+        body: JSON.generate("locale": 'en-us')
+      )
+    return connection.body
+  end
+
+  def getProfile
+    profil =
+      Excon.get(
+        'https://eastus.api.cognitive.microsoft.com/speaker/verification/v2.0/text-independent/profiles/"dc53bdab-feef-4310-aefd-b5f8581ddd6e",',
+        headers: {
+          'Content-Type' => 'application/json',
+          'Ocp-Apim-Subscription-Key' => "#{ENV['AZURE_KEY']}"
+        },
+        body: JSON.generate("locale": 'en-us')
+      )
+      return profil.body
+  end
