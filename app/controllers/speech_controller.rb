fccb3269b1d1375b1c944e6a6eb3868a664e7416
meg635 <marie.eve.goupil@gmail.com>
2021-04-22 15:26:52 -0400

    speech controller

--- app/controllers/speech_controller.rb
+++ app/controllers/speech_controller.rb
@@ -16,20 +16,14 @@ class SpeechController < ApplicationController
         end
         
     end
-
+####################################### GET PROFILE FROM DATABASE ###################
     def speech
+      
         @profile = ProfileId.all
-        #puts @profile
     end
-    #speech();
-
-    def new
-        @speech = Speech.new
-        puts @speech
-        # @speech = params["file_name"]
-    end
-
+####################################### GET PROFILE FROM AZURE SERVICES ###################
     def get_profile
+      
         profil =
         Excon.get(
           'https://eastus.api.cognitive.microsoft.com/speaker/identification/v2.0/text-independent/profiles',
@@ -39,11 +33,6 @@ class SpeechController < ApplicationController
           },
           body: JSON.generate("locale": 'en-us')
         )
-        puts "======="
-        puts profil.body
-        @profilTest = profil.body
-        puts "======="
-        
         return profil.body
         parsed = JSON.parse(profil.body)
             return parsed['profiles']
@@ -51,12 +40,10 @@ class SpeechController < ApplicationController
             puts "Error: #{e}"
 
     end
-
-
+####################################### CREATE AND ENROLL NEW PROFILE ###################
     def enrollment
-      user_name = params[:user_name]
       
-        connection =
+      connection =
         Excon.post(
           'https://eastus.api.cognitive.microsoft.com/speaker/identification/v2.0/text-independent/profiles',
           headers: {
@@ -65,25 +52,22 @@ class SpeechController < ApplicationController
           },
           body: JSON.generate("locale": 'en-us')
         )
-        puts"==============================="
-        puts params
-        parsed = JSON.parse(connection.body)
-        puts user_name
-      @profile = ProfileId.new
-      @profile.user_name = user_name
-      @profile.profile_id = parsed["profileId"]
-      @profile.save
+        @parsed = JSON.parse(connection.body)
+      create_DB_profile();
       create_profile();
-      
     end
-
+    ####################################### CREATE PROFILE IN DATABASE ###################
+    def create_DB_profile
+      
+      @profile = ProfileId.new
+      @profile.user_name = params[:user_name]
+      @profile.profile_id = @parsed["profileId"]
+      @profile.save
+    end
+####################################### ENROLL PROFILE ###################
      def create_profile
-      puts "create profile"
-      puts "-----------------------------"
-      puts params
-      puts "-----------------------------"
+      
       file = params[:enrollment_file]
-      puts file
 
       enroll = Excon.post("https://eastus.api.cognitive.microsoft.com/speaker/identification/v2.0/text-independent/profiles/#{@profile.profile_id}/enrollments",
       headers: {
@@ -94,40 +78,28 @@ class SpeechController < ApplicationController
       body: file,
       )
       
-     puts enroll.body
+      #puts enroll.body
       #puts "==============enroll"
       return enroll.body
     end
-    
-   
-
+####################################### IDENTIFY PROFILE FROM AUDIO FILE ###################
     def identification
-
-
-        puts "-----------------------------"
-        puts params
-        puts "-----------------------------"
-        
-          file = params[:identification_file]
-          puts file
-          profileid = params[:profile_id]
-          puts "================================"
-          puts profileid
+      
+      file = params[:identification_file]
+        # puts file
+        profileid = params[:profile_id]
+        # puts "================================"
+        # puts profileid
           
-          speaker = Excon.post("https://eastus.api.cognitive.microsoft.com/speaker/identification/v2.0/text-independent/profiles/identifySingleSpeaker?profileIds=" + profileid.to_s,
-              headers:{
-                  'Content-Type' => 'audio/wave',
-                  'Ocp-Apim-Subscription-Key' => "3c43bca9ad884fe39518a5cf3925e707"
-              },
-              body: file,
-            )
-            puts speaker.body
-            puts @speech
-            
-            return JSON.parse(speaker.body)
-          
- 
-          
+        speaker = Excon.post("https://eastus.api.cognitive.microsoft.com/speaker/identification/v2.0/text-independent/profiles/identifySingleSpeaker?profileIds=" + profileid.to_s,
+          headers:{
+            'Content-Type' => 'audio/wave',
+            'Ocp-Apim-Subscription-Key' => "3c43bca9ad884fe39518a5cf3925e707"
+          },
+            body: file,
+          )
+        puts speaker.body
+        return JSON.parse(speaker.body)
     end  
 
    
